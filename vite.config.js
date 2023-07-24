758c736782ca74ff219384ae872761faed5f3129
Johandika <55685350+Johandika@users.noreply.github.com>
2023-07-24 13:36:17 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/main.jsx',
+        // ...
+      },
+      output: {
+        onwarn: (warning, warn) => {
+          // Mengabaikan error saat membangun
+          if (warning.code === 'CIRCULAR_DEPENDENCY') return;
+          warn(warning);
+        },
+      },
+    },
+  },
+});
+
+import { defineConfig } from 'vite';
