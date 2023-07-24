bcbda60f607f9fb964400ecf393373b0f45c129b
Johandika <noviamarlina17@gmail.com>
2023-07-24 11:48:47 +0700

    feat: add contact feature and improve hero section style

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -39,10 +39,10 @@ const FeedbackCard = ({ index, testimonial, name, designation, company, image })
 const Feedbacks = () => {
   return (
     <div className="mt-12 bg-black-100 rounded-[20px]">
-      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
+      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[230px] sm:min-h-[300px]`}>
         <motion.div variants={textVariant}>
           <p className={styles.sectionSubText}>What others say</p>
-          <h2 className={styles.heroHeadText}>Testimonials.</h2>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
       </div>
       <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
